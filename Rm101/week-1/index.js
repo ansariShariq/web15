cb8b4899b4f40e01c05d2e9f3d4129aae0d5142e
Shariq Ansari <ansarisharique0003@gmail.com>
2022-05-17 16:29:33 +0530

    u6 sec commit

--- Rm101/week-1/index.js
+++ Rm101/week-1/index.js
@@ -11,4 +11,24 @@ console.log('5'<6)//true first try to convert into a number
 console.log(5<'Shariq')//if one is number and another can't be converted into a number (Nan ) hemce false
 console.log('123'<'124z')//true because there is to string and one can not be both cannot be converted into a number then unicode value compared
 console.log(123<'124p')//false cause one is a number and other can't be converted to a number then num<NaN line Number 11
-console.log('Bignint',5n == 5)
+console.log('Bignint',5n == 5)//loosely equal
+
+
+
+let k = 5
+console.log('left shift')
+k = k << 3545653554
+console.log(k)
+
+console.log('right  shift')
+k >>= 7
+console.log(k)
+
+
+//This operator is represented by x &&= y, and it is called the logical AND assignment operator. 
+//It assigns the value of y into x only if x is a truthy value
+
+
+
+//bitwise operator
+console.log('Bitwise Operator')
